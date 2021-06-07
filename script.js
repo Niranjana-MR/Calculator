061a1267b1ef1ab36822452f6f9bfe32b382685d
Niranjana M R <niranjan.mr99@gmail.com>
2021-06-07 12:28:24 +0530

    added event listeners for button clicks. Display values. Clear and Delete button works as intended

--- script.js
+++ script.js
@@ -1,32 +1,71 @@
+main();
 
-//perform check for num1 and num2 before passing 
+function main() {
 
-function add(num1, num2){
+    const display = document.querySelector('#display');
+    const calculator = document.querySelector('#calculator');
+
+    let operator = '';
+    let operand = '';
+    let calc = '';
+    let str = '';
+
+    calculator.addEventListener('click', (event) => {
+        const isButton = event.target.nodeName === 'BUTTON';
+        if (isButton) {
+            let input = (event.target.innerHTML);
+            if (!isNaN(input)) {
+                operand += input;
+                display.value += (event.target.innerHTML);
+            } else {
+                if (event.target.innerHTML === 'CLEAR') {
+                    display.value = '';
+                } else if (event.target.innerHTML === 'DELETE') {
+                    if (display.value !== '')
+                        str = display.value;
+                        display.value = str.slice(0, -1);
+                } else {
+                    operator = input;
+                    calc = operand + operator;
+                    display.value += (event.target.innerHTML);
+                }
+            }
+        }
+    });
+}
+
+
+function add(num1, num2) {
     return num1 + num2;
 }
 
-function subtract(num1, num2){
+function subtract(num1, num2) {
     return num1 - num2;
 }
 
-function multiply(num1, num2){
+function multiply(num1, num2) {
     return num1 * num2;
 }
 
-function divide(num1, num2){
+function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(op, num1, num2){
-    switch(op){
-        case '+': add(num1, num2);
-                  break;
-        case '-': subtract(num1, num2);
-                  break;
-        case '*': multiply(num1, num2);
-                  break;
-        case '/': divide(num1, numm2);
-                  break;
-        default: console.log('Oops, something went wrong');
+function operate(op, num1, num2) {
+    switch (op) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+        default:
+            console.log('Oops, something went wrong');
     }
 }
