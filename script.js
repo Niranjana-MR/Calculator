5634c2ac58937ad290d2d20615eb6fcf1c25b524
Niranjana M R <niranjan.mr99@gmail.com>
2021-06-07 20:34:01 +0530

    fixed bug with decimal operation

--- script.js
+++ script.js
@@ -1,137 +1,18 @@
-main();
-
-function main() {
-
-    const display = document.querySelector('#display');
-    display.setAttribute('disabled', true);
-    display.value = '';
-
-    const calculator = document.querySelector('#calculator');
-
-    const decimal = document.querySelector('#decimal');
-
-    let operator = '';
-    let value = '';
-    let operand1 = '';
-    let operand2 = '';
-    let str = '';
-    let result = 0;
-
-    document.addEventListener('keyup', (e) => {
-        let key = e.key;
-        triggerKey(key);
-    });
-
-    calculator.addEventListener('click', (event) => {
-
-        const isButton = event.target.nodeName === 'BUTTON';
-
-        if (isButton) {
-            let input = (event.target.innerHTML);
-            if (!isNaN(input)) {
-                value += input;
-                display.value += (event.target.innerHTML);
-            } else { //Enter if input isn't a number
-
-                //if clear button is pressed
-                if (event.target.innerHTML === 'CLEAR') {
-                    operand1 = '';
-                    operand2 = '';
-                    operator = '';
-                    value = '';
-                    result = 0;
-                    decimal.disabled = false;
-                    display.value = '';
-                }
-
-                //if delete button is pressed
-                else if (event.target.innerHTML === 'DELETE') {
-                    if (display.value !== '') {
-                        str = display.value;
-                        display.value = str.slice(0, -1);
-                        value = value.slice(0, -1);
-                        if( display.value === ''){
-                            document.querySelector('#clearBtn').click();
-                        }
-                    }
-                }
-
-                //evulate the result
-                else if (event.target.innerHTML === '=') {
-                    if (operand2 === '') {
-                        operand2 = value;
-                        value = '';
-                    }
-                    result = operate(operator, operand1, operand2);
-                    if (typeof (result) === 'undefined') {
-                        result = 0;
-                    } else if (isNaN(result)) {
-                        result = 0;
-                    }
-                    operand1 = result;
-                    operand2 = '';
-                    operator = '';
-                    display.value = operand1;
-                }
-
-                //for any other operations
-                else {
-
-                    //for decimal value
-                    if (input === '.') {
-                            value += '.';
-                            decimal.disabled = true;
-                    }
-
-                    //store first operand if empty 
-                    else if (operand1 === '') {
-                        operand1 = value;
-                        value = '';
-                        operator = input;
-                        decimal.disabled = false;
-                    }
-
-                    //store operator value if empty
-                    else if (operator === '') {
-                        operator = input;
-                        decimal.disabled = false;
-                    }
-
-                    //store second operand if empty and operator isn't empty
-                    else if (operand2 === '' && operator !== '') {
-                        operand2 = value;
-                        value = '';
-                        result = operate(operator, operand1, operand2);
-                        operator = input;
-                        decimal.disabled = false;
-                        operand1 = result.toString();
-                        display.value = '';
-                        display.value += operand1;
-                        operand2 = '';
-                    }
-                    display.value += (event.target.innerHTML);
-                }
-            }
-        }
-    });
-}
-
-
 function add(num1, num2) {
-    let addRes = num1+num2;
-    addRes = Math.round(addRes*1000)/1000;
+    let addRes = num1 + num2;
+    addRes = Math.round(addRes * 1000) / 1000;
     return addRes;
 }
 
 function subtract(num1, num2) {
-    let subtractRes = num1-num2;
-    subtractRes = Math.round(subtractRes*1000)/1000;
+    let subtractRes = num1 - num2;
+    subtractRes = Math.round(subtractRes * 1000) / 1000;
     return subtractRes;
 }
 
 function multiply(num1, num2) {
-    let multiplyRes = num1*num2;
-    multiplyRes = Math.round(multiplyRes*1000)/1000;
+    let multiplyRes = num1 * num2;
+    multiplyRes = Math.round(multiplyRes * 1000) / 1000;
     return multiplyRes;
 }
 
@@ -139,8 +20,8 @@ function divide(num1, num2) {
     if (num2 === 0) {
         return 0;
     }
-    let divideRes = num1/num2;
-    divideRes = Math.round(divideRes*1000)/1000;
+    let divideRes = num1 / num2;
+    divideRes = Math.round(divideRes * 1000) / 1000;
     return divideRes;
 }
 
@@ -170,7 +51,7 @@ function operate(op, num1, num2) {
     }
 }
 
-function triggerKey(key){
+function triggerKey(key) {
     if (!isNaN(key)) {
         document.querySelector(`#num${key}`).click();
     } else {
@@ -212,3 +93,130 @@ function triggerKey(key){
         }
     }
 }
+
+function main() {
+
+    const display = document.querySelector('#display');
+    display.setAttribute('disabled', true);
+    display.value = '';
+
+    const calculator = document.querySelector('#calculator');
+
+    const decimal = document.querySelector('#decimal');
+
+    let operator = '';
+    let value = '';
+    let operand1 = '';
+    let operand2 = '';
+    let str = '';
+    let result = 0;
+
+    document.addEventListener('keyup', (e) => {
+        let key = e.key;
+        triggerKey(key);
+    });
+
+    calculator.addEventListener('click', (event) => {
+
+        const isButton = event.target.nodeName === 'BUTTON';
+
+        if (isButton) {
+            let input = (event.target.innerHTML);
+            if (!isNaN(input)) 
+            //Enter when input is a number
+            {
+                value += input;
+                display.value += (event.target.innerHTML);
+            } 
+
+            //Enter if input isn't a number
+            else { 
+
+                //if clear button is pressed
+                if (event.target.innerHTML === 'CLEAR') {
+                    operand1 = '';
+                    operand2 = '';
+                    operator = '';
+                    value = '';
+                    result = 0;
+                    decimal.disabled = false;
+                    display.value = '';
+                }
+
+                //if delete button is pressed
+                else if (event.target.innerHTML === 'DELETE') {
+                    if (display.value !== '') {
+                        str = display.value;
+                        display.value = str.slice(0, -1);
+                        value = value.slice(0, -1);
+                        if (display.value === '') {
+                            document.querySelector('#clearBtn').click();
+                        }
+                    }
+                }
+
+                //evulate the result
+                else if (event.target.innerHTML === '=') {
+                    if (operand2 === '') {
+                        operand2 = value;
+                        value = '';
+                    }
+                    result = operate(operator, operand1, operand2);
+                    if (typeof (result) === 'undefined') {
+                        result = 0;
+                    } else if (isNaN(result)) {
+                        result = 0;
+                    }
+                    operand1 = result.toString();
+                    let decimalExists = operand1.indexOf('.');
+                    if(decimalExists>=0){
+                        decimal.disabled = true;
+                    }
+                    operand2 = '';
+                    operator = '';
+                    display.value = operand1;
+                }
+
+                //for any other operations
+                else {
+
+                    //for decimal value
+                    if (input === '.') {
+                        decimal.disabled = true;
+                        value += '.';
+                    }
+
+                    //store first operand if empty 
+                    else if (operand1 === '') {
+                        operand1 = value;
+                        value = '';
+                        operator = input;
+                        decimal.disabled = false;
+                    }
+
+                    //store operator value if empty
+                    else if (operator === '') {
+                        operator = input;
+                        decimal.disabled = false;
+                    }
+
+                    //store second operand if empty and operator isn't empty
+                    else if (operand2 === '' && operator !== '') {
+                        operand2 = value;
+                        value = '';
+                        result = operate(operator, operand1, operand2);
+                        operator = input;
+                        decimal.disabled = false;
+                        operand1 = result.toString();
+                        display.value = '';
+                        display.value += operand1;
+                        operand2 = '';
+                    }
+                    display.value += (event.target.innerHTML);
+                }
+            }
+        }
+    });
+}
+
+main();
